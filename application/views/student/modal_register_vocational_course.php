328f3b50ea1746b02bc1497da5f8f834f3db9bc8
nikita <nikita.patel@searchnative.in>
2016-06-09 11:42:12 +0530

    end div at last

--- application/views/student/modal_register_vocational_course.php
+++ application/views/student/modal_register_vocational_course.php
@@ -106,7 +106,7 @@
             </div>
             <!-- row --> 
         </div>
-
+    </div>
         <script src="<?php echo base_url(); ?>assets/js/jquery-1.9.1.js"></script>
 
         <script>
