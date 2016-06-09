517449fcf1c1b1de30b1ad518f9cdd59625e0018
nikita <nikita.patel@searchnative.in>
2016-06-09 13:45:08 +0530

    display registered student according to vocational course

--- application/models/admin/Crud_model.php
+++ application/models/admin/Crud_model.php
@@ -1637,9 +1637,11 @@ class Crud_model extends CI_Model {
      * vocational course student list
      * return mixed data
      */
-    function get_vocational_student() {
+    function get_vocational_student($id) {
+            
         return $this->db->select('vocational_course_fee.*, student.*, vocational_course.*,course_category.*')
                         ->from('vocational_course_fee')
+                        ->where('vocational_course_fee.vocational_course_id',$id)
                         ->join('student', 'student.std_id = vocational_course_fee.student_id')
                         ->join('vocational_course', 'vocational_course.vocational_course_id = vocational_course_fee.vocational_course_id')
                         ->join('course_category', 'course_category.category_id = vocational_course.category_id')
