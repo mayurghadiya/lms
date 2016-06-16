b77c012970079225ed87c951300fb558fc21c4e9
Mayur Ghadiya <mayur.ghadiya@searchnative.in>
2016-06-16 18:10:15 +0530

    demo 16-06-2016

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
  */
 ini_set('memory_limit', '-1');
 
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  * ---------------------------------------------------------------
